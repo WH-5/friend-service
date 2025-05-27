049f101a13c390ceb1c80bb76d6d592effa7473c
wh <wang.hui_fox@foxmail.com>
2025-05-26 22:39:44 -0400

    鉴权开关

--- internal/server/grpc.go
+++ internal/server/grpc.go
@@ -3,7 +3,6 @@ package server
 import (
 	v1 "github.com/WH-5/friend-service/api/friend/v1"
 	"github.com/WH-5/friend-service/internal/conf"
-	"github.com/WH-5/friend-service/internal/middleware"
 	"github.com/WH-5/friend-service/internal/service"
 	"github.com/go-kratos/kratos/v2/middleware/logging"
 	"github.com/go-kratos/kratos/v2/middleware/validate"
@@ -17,7 +16,7 @@ import (
 func NewGRPCServer(c *conf.Server, friendService *service.FriendService, logger log.Logger) *grpc.Server {
 	var opts = []grpc.ServerOption{
 		grpc.Middleware(
-			middleware.AuthCheckExist(friendService),
+			//middleware.AuthCheckExist(friendService),
 			recovery.Recovery(),
 			logging.Server(logger),
 			validate.Validator(),
